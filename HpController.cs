18230701c348a179e22c81adc637258c4926fdbc
hyunjin813 <chj061649@gmail.com>
2021-06-07 09:30:37 +0900

    Add files via upload

---
+++ HpController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpController : MonoBehaviour
+{
+
+public float totalOxygen;
+public float currentOxygen;
+
+public Text text_total;
+public Text text_current;
+public Image image_gauge;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentOxygen -= Time.deltaTime;
+        text_current.text = Mathf.RoundToInt(currentOxygen).ToString();
+        image_gauge.fillAmount = currentOxygen / totalOxygen;
+        text_total.text = totalOxygen.ToString();
+
+    }
+
+    
+}
