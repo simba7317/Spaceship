3edd74f3b5479ac6c4a3542e85bb09026b408940
simba7317 <70802630+simba7317@users.noreply.github.com>
2021-06-10 16:41:35 +0900

    Add files via upload

---
+++ Hp.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hp : MonoBehaviour
+{
+
+    public float totalOxygen;
+    public float currentOxygen;
+
+    public Text text_total;
+    public Text text_current;
+    public Image image_gauge;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
