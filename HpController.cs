927ec1fa1cc5bb4fa6c36fe55288c1b034d2668f
simba7317 <70802630+simba7317@users.noreply.github.com>
2021-06-12 00:14:53 +0900

    Update HpController.cs

--- HpController.cs
+++ HpController.cs
@@ -5,24 +5,52 @@ using UnityEngine.UI;
 
 public class HpController : MonoBehaviour
 {
+    // 체력
+    [SerializeField]
+    private int hp;
+    private int currentHp;
 
-public float totalOxygen;
-public float currentOxygen;
+    // 필요한 이미지
+    [SerializeField]
+    private Image[] images_Gauge;
 
-public Text text_total;
-public Text text_current;
-public Image image_gauge;
+    private const int HP = 0;
 
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        currentHp = hp;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        currentOxygen -= Time.deltaTime;
-        text_current.text = Mathf.RoundToInt(currentOxygen).ToString();
-        image_gauge.fillAmount = currentOxygen / totalOxygen;
-        text_total.text = totalOxygen.ToString();
-
+        GaugeUpdate();
     }
 
-    
+    private void GaugeUpdate()
+    {
+        images_Gauge[HP].fillAmount = (float)currentHp / hp;
+    }
+
+    public void IncreaseHP(int _count)
+    {
+        if (currentHp + _count < hp)
+            currentHp += _count;
+        else
+            currentHp = hp;
+    }
+
+    public void DecreaseHP(int _count)
+    {
+        currentHp -= _count;
+
+        if (currentHp <= 0)
+            //나중에 게임 오버 씬 전환하기!
+    }
 }
